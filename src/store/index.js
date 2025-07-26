207c0070da86447e9c605df62a4ce5b7a533af61
Behruz <2007behruz@gmail.com>
2025-07-26 21:32:25 +0500

    final

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import githubData from './github/githubData'
-
-
-export const store = configureStore({
-    reducer:{
-        github: githubData
-    }
-})
